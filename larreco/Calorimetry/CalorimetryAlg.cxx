2169826aefd4e4d2cf756e0550934238a0b2e4d1
Edward <e.tyley@sheffield.ac.uk>
2020-04-03 11:59:10 -0500

    Overloaded CalorimetryAlg to allow for local E field recombination factor when calculating dEdx

--- larreco/Calorimetry/CalorimetryAlg.cxx
+++ larreco/Calorimetry/CalorimetryAlg.cxx
@@ -21,9 +21,9 @@ namespace calo{
   //--------------------------------------------------------------------
   CalorimetryAlg::CalorimetryAlg(const Config& config)
   {
-     this->reconfigure(config);
+    this->reconfigure(config);
 
-     detprop = art::ServiceHandle<detinfo::DetectorPropertiesService const>()->provider();
+    detprop = art::ServiceHandle<detinfo::DetectorPropertiesService const>()->provider();
   }
 
   //--------------------------------------------------------------------
@@ -36,7 +36,7 @@ namespace calo{
   void   CalorimetryAlg::reconfigure(const Config& config)
   {
 
-    fCalAmpConstants 	= config.CalAmpConstants();
+    fCalAmpConstants  = config.CalAmpConstants();
     fCalAreaConstants   = config.CalAreaConstants();
     fUseModBox          = config.CaloUseModBox();
     fLifeTimeForm       = config.CaloLifeTimeForm();
@@ -50,32 +50,48 @@ namespace calo{
   // ----------------------------------------------------------------------------------//
   double CalorimetryAlg::dEdx_AMP(art::Ptr< recob::Hit >  hit, double pitch, double T0) const
   {
-    return dEdx_AMP(hit->PeakAmplitude()/pitch, hit->PeakTime(), hit->WireID().Plane, T0);
+    return dEdx_AMP(hit, pitch, T0, detprop->Efield());
+  }
+  double CalorimetryAlg::dEdx_AMP(art::Ptr< recob::Hit >  hit, double pitch, double T0, double Efield) const
+  {
+    return dEdx_AMP(hit->PeakAmplitude()/pitch, hit->PeakTime(), hit->WireID().Plane, T0, Efield);
   }
 
   // ----------------------------------------------------------------------------------//
   double CalorimetryAlg::dEdx_AMP(recob::Hit const&  hit, double pitch, double T0) const
   {
-    return dEdx_AMP(hit.PeakAmplitude()/pitch, hit.PeakTime(), hit.WireID().Plane, T0);
+    return dEdx_AMP(hit, pitch, T0, detprop->Efield());
+  }
+  double CalorimetryAlg::dEdx_AMP(recob::Hit const&  hit, double pitch, double T0, double Efield) const
+  {
+    return dEdx_AMP(hit.PeakAmplitude()/pitch, hit.PeakTime(), hit.WireID().Plane, T0, Efield);
   }
 
   ///\todo The plane argument should really be for a view instead
   // ----------------------------------------------------------------------------------//
   double CalorimetryAlg::dEdx_AMP(double dQ, double time, double pitch, unsigned int plane, double T0) const
   {
+    return dEdx_AMP(dQ, time, pitch, plane, T0, detprop->Efield());
+  }
+  double CalorimetryAlg::dEdx_AMP(double dQ, double time, double pitch, unsigned int plane, double T0, double Efield) const
+  {
     double dQdx   = dQ/pitch;           // in ADC/cm
-    return dEdx_AMP(dQdx, time, plane, T0);
+    return dEdx_AMP(dQdx, time, plane, T0, Efield);
   }
 
   // ----------------------------------------------------------------------------------//
   double CalorimetryAlg::dEdx_AMP(double dQdx,double time, unsigned int plane, double T0) const
+  {
+    return dEdx_AMP(dQdx, time, plane, T0, detprop->Efield());
+  }
+  double CalorimetryAlg::dEdx_AMP(double dQdx,double time, unsigned int plane, double T0, double Efield) const
   {
     double fADCtoEl=1.;
 
     fADCtoEl = fCalAmpConstants[plane];
 
     double dQdx_e = dQdx/fADCtoEl;  // Conversion from ADC/cm to e/cm
-    return dEdx_from_dQdx_e(dQdx_e,time, T0);
+    return dEdx_from_dQdx_e(dQdx_e,time, T0, Efield);
   }
 
   //------------------------------------------------------------------------------------//
@@ -83,11 +99,19 @@ namespace calo{
   // ----------------------------------------------------------------------------------//
   double CalorimetryAlg::dEdx_AREA(art::Ptr< recob::Hit >  hit, double pitch, double T0) const
   {
+    return dEdx_AREA(hit, pitch, T0, detprop->Efield());
+  }
+  double CalorimetryAlg::dEdx_AREA(art::Ptr< recob::Hit >  hit, double pitch, double T0, double Efield) const
+  {
     return dEdx_AREA(hit->Integral()/pitch, hit->PeakTime(), hit->WireID().Plane, T0);
   }
 
   // ----------------------------------------------------------------------------------//
   double CalorimetryAlg::dEdx_AREA(recob::Hit const&  hit, double pitch, double T0) const
+  {
+    return dEdx_AREA(hit, pitch, T0, detprop->Efield());
+  }
+  double CalorimetryAlg::dEdx_AREA(recob::Hit const&  hit, double pitch, double T0, double Efield) const
   {
     return dEdx_AREA(hit.Integral()/pitch, hit.PeakTime(), hit.WireID().Plane, T0);
   }
@@ -95,12 +119,20 @@ namespace calo{
   // ----------------------------------------------------------------------------------//
   double CalorimetryAlg::dEdx_AREA(double dQ,double time, double pitch, unsigned int plane, double T0) const
   {
+    return dEdx_AREA(dQ, time, pitch, plane, T0, detprop->Efield());
+  }
+  double CalorimetryAlg::dEdx_AREA(double dQ,double time, double pitch, unsigned int plane, double T0, double Efield) const
+  {
     double dQdx   = dQ/pitch;           // in ADC/cm
     return dEdx_AREA(dQdx, time, plane, T0);
   }
 
   // ----------------------------------------------------------------------------------//
   double CalorimetryAlg::dEdx_AREA(double dQdx,double time, unsigned int plane, double T0) const
+  {
+    return dEdx_AREA(dQdx, time, plane, T0, detprop->Efield());
+  }
+  double CalorimetryAlg::dEdx_AREA(double dQdx,double time, unsigned int plane, double T0, double Efield) const
   {
     double fADCtoEl=1.;
 
@@ -113,12 +145,16 @@ namespace calo{
   // ----------------- apply Lifetime and recombination correction.  -----------------//
   double CalorimetryAlg::dEdx_from_dQdx_e(double dQdx_e, double time, double T0) const
   {
+    return dEdx_from_dQdx_e(dQdx_e, time, T0, detprop->Efield());
+  }
+  double CalorimetryAlg::dEdx_from_dQdx_e(double dQdx_e, double time, double T0, double Efield) const
+  {
     if (fDoLifeTimeCorrection)
       dQdx_e *= LifetimeCorrection(time, T0);   // Lifetime Correction (dQdx_e in e/cm)
     if(fUseModBox) {
-      return detprop->ModBoxCorrection(dQdx_e);
+      return detprop->ModBoxCorrection(dQdx_e, Efield);
     } else {
-      return detprop->BirksCorrection(dQdx_e);
+      return detprop->BirksCorrection(dQdx_e, Efield);
     }
   }
 
